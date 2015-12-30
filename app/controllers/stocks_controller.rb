889677b8742bc4591c911d225aec347e8698a1e0
gVien <wavetech86@gmail.com>
2015-12-29 19:32:29 -0800

    improve autocomplete by filter stock array in model

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,13 +2,15 @@ class StocksController < ApplicationController
   # include ApplicationHelper
 
   def index
-    render json: {stocks: Stock.stock_autocomplete_array}
+    respond_to do |format|
+      format.html
+      format.json { render json: {stocks: Stock.stock_autocomplete_array(params[:term])} }
+    end
   end
 
   def create
     # default value of period is 30 days if it is left blanked
     params[:period] = 30 if params[:period] == ""
-
     if request.xhr?
       @data = YahooFinanceDataCollector.get_price_data(params[:symbol], params[:period].to_i)
       render json: @data
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,18 @@
 class Stock < ActiveRecord::Base
   validates :symbol, presence: true, uniqueness: true
 
-  def self.stock_autocomplete_array
+  def self.searched_stocks(query)
+    if query.blank?
+      results = []
+    else
+      results = where("(symbol LIKE ?) OR (company_name LIKE ?)", "%#{query.upcase}%", "%#{query.capitalize}%")
+    end
+  end
+
+  def self.stock_autocomplete_array(query)
     stock_arr = []
-    self.all.each do |stock|
+
+    searched_stocks(query).each do |stock|
       stock_arr << {
                     :value => stock.symbol,
                     :label => "#{stock.symbol} - #{stock.company_name}"}
